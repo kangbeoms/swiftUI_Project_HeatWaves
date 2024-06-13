6a106f2db5122aeacfde956ffa79d6b552599763
BeomShiek <tvyy11054@gmail.com>
2024-06-13 11:10:35 +0900

    작업물 업데이트

--- Data/지도 정보.py
+++ Data/지도 정보.py
@@ -4,59 +4,40 @@
 설명: 1.database에 있는 랜드마크 이름,위도,경도,해발고도,건물 높이를 불러온다.
      2.제작한 선형회귀모델을 사용 get요청으로 예측값을 보낸다.
 """
-import branca
+# 파이썬 서버 구동
 from flask import Flask, jsonify, render_template, request,current_app
+# json으로 데이터 송신
 import json
 import joblib
+# sql 접근
 import pymysql
+# 파이썬 맵 사용
 import folium
+# json파일 불러오기
 from joblib import dump, load
+#회귀모델 스케일링
 from sklearn.discriminant_analysis import StandardScaler
+# os 명령어 사용
 import os
-
+# 폴리움 지도 옵션 
 from folium.plugins import MousePosition
 from folium.plugins import Search
 from folium.plugins import FloatImage
 
-from PIL import Image
-
+# html 파일 실행
 from jinja2 import Environment, FileSystemLoader
+
+# 플라스크 서버 지정
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False #for utf8
 
 template_dir = os.path.abspath(os.path.dirname(__file__))  # 현재 파일의 절대 경로
+
+# html에 파라미터를 주기위해 jinja2 라이브러리 사용
 env = Environment(loader=FileSystemLoader(template_dir))
 template = env.get_template('jsp/tooltipList.html')
-template_gojsp = env.get_template('jsp/goSwift.jsp')
 
-@app.route("/insert")
-def insert():
-        
-        landname = request.args.get("landname")
-        lat = request.args.get("lat")
-        lng = request.args.get("lng")
-        sealevel = request.args.get("sealevel")
-        height = request.args.get("height")
-        location = request.args.get("location")
-        image = request.args.get("image")
-
-        conn = pymysql.connect(
-        host='127.0.0.1',
-        user='root',
-        password='qwer1234',
-        db='sealevel',
-        charset='utf8'
-    )
-        
-    # Connection으로부터 Cursor 생성
-        curs = conn.cursor()
-
-        # sql 문장
-        sql = "insert into sealevel(landname,lat,lng,sealevel,height,location,image) values (%s,%s,%s,%s,%s,%s,%s)"
-        curs.execute(sql,(landname,lat,lng,sealevel,height,location,image))
-        conn.commit()
-        conn.close()
-
+# 지도에 마커를 표시하기 위한 데이터베이스 진입
 @app.route("/select")
 def select():
     # MySql Connection
@@ -76,16 +57,16 @@ def select():
     curs.execute(sql)
     rows = curs.fetchall()
     conn.close()
-   #print(rows)
 
     result = json.dumps(rows, ensure_ascii=False).encode('utf8')
 
     return result
 
-# 선형회귀모델 
+# 선형회귀모델 예측값 보내기
 @app.route('/getpred')
 def sea():
     datalist = []
+    # 7개의 컬럼 알규먼트 받기 
     datalist.append(float(request.args.get("Year")))
     datalist.append(float(request.args.get("co2")))
     datalist.append(float(request.args.get("Population")))
@@ -98,39 +79,44 @@ def sea():
     # 스케일된 train_input 과 모델을 불러옴
     scale, clf = joblib.load(f"{current_app.root_path}/model_with_scaler.sea")
 
-    #print(inputdata)
     scaled_data = scale.transform(inputdata)
     pre = clf.predict(scaled_data)
-    #print(pre)
+
     pre_int = list(map(int, pre))
     return {'result' : pre_int}
 
+# 서버 테스트용
 @app.route("/test")
 def iris2():
     #print(os.getcwd())
     return '<h1>테스트 서버</h1><br><h2>테스트 서버</h2>'
 
+# 지도 보여주기
 @app.route("/showmap",methods=['GET','POST'])
 def mapview():
     
     # 대한민국 지도
     korea_map = folium.Map(
-    location=[36.5, 127],
+    location=[36, 127.7],
     zoom_start= 7,
-
+    
     )
-    korea_map.fit_bounds([[33, 124.6], [38.6, 131.9]])
-
+    #korea_map.fit_bounds([[33, 124.0], [38.6, 131.9]])
+    korea_map.zoom_start = 7
     savedata = json.loads(select())
-    file_path = os.path.abspath(f"{current_app.root_path}/jsp/tooltipList.html")
 
+    # html파일 읽어오기
+    file_path = os.path.abspath(f"{current_app.root_path}/jsp/tooltipList.html")
     with open(file_path, "r", encoding='utf-8') as file:
         html_content = file.read()
 
     for i in savedata:
-        folium.IFrame(html=html_content, width=500, height=500).add_to(korea_map)
+
+        folium.IFrame(html=html_content, width=3500, height=500).add_to(korea_map)
+        # html파일에 파라미터를 부여해 정보 보이기
         html_content = template.render(name= i[0], height= i[3],imagepath=i[0])
 
+        # 미리 지정한 위치에 마커를 표시하고 클릭 시 html파일로 이동
         folium.Marker(
             [i[1],i[2]],
             popup=html_content,
@@ -139,10 +125,6 @@ def mapview():
             lazy=True,
             ).add_to(korea_map)
 
-    #folium.LayerControl().add_to(korea_map)
-    # 레이어 사용
-    #folium.TileLayer(show=False).add_to(korea_map)
-
     # GeoJSON 파일 열기
     with open(f"{current_app.root_path}/json/korea_map.json", "r") as f:
         geojson_data = json.load(f)
@@ -150,62 +132,52 @@ def mapview():
     style_function = lambda feature: {
         #'fillColor': 'white',  # 예시 채우기 색상
         'color': 'black',  # 경계선 색상
-        'weight': 3,  # 경계선 굵기 (얇게 설정)
+        'weight': 2,  # 경계선 굵기 (얇게 설정)
         'fillOpacity': 0.1,  # 채우기 불투명도
     }
 
-    # GeoJSON 데이터를 폴리움 객체로 변환하여 맵에 추가
-    ser = folium.GeoJson(   
+    #GeoJSON 데이터를 폴리움 객체로 변환하여 맵에 추가
+    folium.GeoJson(   
         geojson_data,
         name="geojson",
         style_function=style_function
     ).add_to(korea_map)
+
+
+    # 모바일 환경과 맞지않아 사용안함
     MousePosition().add_to(korea_map)
 
-    Search(
-        layer=ser,
-        geom_type="Polygon",
-        placeholder="지역 검색",
-        collapsed=False,
-        search_label='CTP_KOR_NM',
-        weight=10,
-        search_zoom= 10
-
-    ).add_to(korea_map)
-
-    url = (" https://raw.githubusercontent.com/ocefpaf/secoora_assets_map/"
-        "a250729bbcf2ddd12f46912d36c33f7539131bec/secoora_icons/rose.png")
-    FloatImage(url, bottom=65, left=80).add_to(korea_map)
-
-    css = """
-    <style>
-        .leaflet-control-search {
-            width: 250px;
-        }
-        .leaflet-control-search input {
-            width: 200px;
-        }
-    </style>
-    """
-
-    # 현재 나의 위치 표시
-    folium.plugins.LocateControl(auto_start=False,setView=False,drawCircle=False).add_to(korea_map)
-
-    korea_map.get_root().html.add_child(folium.Element(css))
 
     return korea_map.get_root().render()
 
-# 예측페이지로 이동 루트
 @app.route("/goSwiftfile",methods=['GET','POST'])
 def goswift():
+
     name = request.args.get('name')
-    #swift_get = template_gojsp.render(name= name)
-    print("가져온이름",name)
 
-    return {'result' : name}
+    # MySql Connection
+    conn = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='qwer1234',
+        db='sealevel',
+        charset='utf8'
+    )
 
+    # Connection으로부터 Cursor 생성
+    curs = conn.cursor()
+
+    # sql 문장
+    sql = "select * from mappoint where landname=%s"
+    curs.execute(sql,(name))
+    rows = curs.fetchall()
+    conn.close()
+
+    # response = jsonify({'result': name})
+    # response.headers.add('Content-Type', 'application/json; charset=utf-8')
+
+    return json.dumps(rows, ensure_ascii=False).encode('utf8')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=5000, debug=True)
-
 
