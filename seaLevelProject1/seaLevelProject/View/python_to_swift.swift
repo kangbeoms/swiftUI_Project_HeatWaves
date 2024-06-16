a91fdb007d7918e2ba1495c1fc5bbb432169b1b4
tom3017 <tom3017@naver.com>
2024-06-16 13:21:36 +0900

    2024-06-16 MapMarker 수정중 Marker 클릭시 제스쳐 구현중

--- seaLevelProject1/seaLevelProject/View/python_to_swift.swift
+++ seaLevelProject1/seaLevelProject/View/python_to_swift.swift
@@ -5,31 +5,31 @@
 //  Created by 김수진 on 6/13/24.
 //
 
-import SwiftUI
-
-struct python_to_swift: View {
-    
-    @State var selectLandMark: [GetLandMark] = []
-    
-    var body: some View {
-        Detail()
-            .onAppear(perform: {
-                let json = GetLandMarkJSON()
-                Task {
-                    selectLandMark = try await json.loadJsonData(url:"http://127.0.0.1:5000/goSwiftfile?name=63빌딩")
-//                    for item in selectLandMark {
-//                        print("건물이름 : \(item.name) \nlat :\(item.lat)\nlng:\(item.lng)")
-                        print("건물이름 : \($selectLandMark.result)")
-//                    }
-//                    print(json)
-                }
-            })
-        
-
-    }
-
-}
-
-#Preview {
-    python_to_swift()
-}
+//import SwiftUI
+//
+//struct python_to_swift: View {
+//    
+//    @State var selectLandMark: [GetLandMark] = []
+//    
+//    var body: some View {
+//        Detail()
+//            .onAppear(perform: {
+//                let json = GetLandMarkJSON()
+//                Task {
+//                    selectLandMark = try await json.loadJsonData(url:"http://127.0.0.1:5000/goSwiftfile?name=63빌딩")
+////                    for item in selectLandMark {
+////                        print("건물이름 : \(item.name) \nlat :\(item.lat)\nlng:\(item.lng)")
+//                        print("건물이름 : \($selectLandMark.result)")
+////                    }
+////                    print(json)
+//                }
+//            })
+//        
+//
+//    }
+//
+//}
+//
+//#Preview {
+//    python_to_swift()
+//}
